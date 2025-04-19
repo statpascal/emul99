495314699b0f5d2e3dd0cb714c93c657b6e2e2b8
statpascal <git@greylags.de>
2025-04-19 16:59:09 +0200

    V 0.1 Beta 10

--- src/ti99.pas
+++ src/ti99.pas
@@ -5,7 +5,7 @@ uses cthreads, gtk3, cfuncs, sdl2, timer, memmap,
 
 const
     KeyMapSize = 256;
-    VersionString = '0.1 beta 9';
+    VersionString = '0.1 beta 10';
     WindowTitle = 'Emul99';
 
 type
