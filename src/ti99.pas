d5f099df7002d15436a4a5ea76577d5cd3ad7152
statpascal <git@greylags.de>
2025-05-09 11:05:24 +0200

    PAB logs for Open/Close

--- src/ti99.pas
+++ src/ti99.pas
@@ -5,7 +5,7 @@ uses cthreads, gtk3, cfuncs, sdl2, timer, memmap, sysutils, fileop,
 
 const
     KeyMapSize = 256;
-    VersionString = '0.2 Beta 5';
+    VersionString = '0.2 Beta 6';
     WindowTitle = 'Emul99';
 
 type
