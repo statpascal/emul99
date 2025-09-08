d6eb931dd9836516df6d296c457069cb5f3fb0f6
statpascal <git@greylags.de>
2025-09-08 18:53:43 +0200

    Fixed but in host system text files

--- src/ti99.pas
+++ src/ti99.pas
@@ -5,7 +5,7 @@ uses cthreads, gtk3, cfuncs, sdl2, timer, memmap, sysutils, fileop,
 
 const
     KeyMapSize = 256;
-    VersionString = '0.2';
+    VersionString = '0.2.1';
     WindowTitle = 'Emul99';
 
 type
